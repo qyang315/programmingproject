5f2d78fd50af470d74b4be45f26f5df90c0cc46f
qyang315 <98200962+qyang315@users.noreply.github.com>
2022-01-30 20:45:13 -0500

    Update main.py

--- main.py
+++ main.py
@@ -28,29 +28,3 @@ def get_meme(sub,count):
         result.append(temp)
 
     return result
-
-def get_text(sub,count):
-    sub_reddit = reddit.subreddit(sub)
-    hot_meme = sub_reddit.hot(limit=count)
-    result=[]
-    textP= "No selftext Present"
-    for submission in hot_meme:
-        if(submission.selftext):
-            temp = {"Title": submission.title,
-                    "text": submission.selftext,
-                    "Upvotes": submission.ups,
-                    "Downvotes": submission.downs,
-                    "Redditurl": submission.shortlink,
-                    "Subreddit": sub
-                    }
-        else:
-            temp = {"Title": submission.title,
-                    "text": textP,
-                    "Upvotes": submission.ups,
-                    "Downvotes": submission.downs,
-                    "Redditurl": submission.shortlink,
-                    "Subreddit": sub
-                    }
-
-        result.append(temp)
-    return result
