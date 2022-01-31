777a9b5837c801f2c8fafdab74199f993b0be8ca
qyang315 <98200962+qyang315@users.noreply.github.com>
2022-01-30 20:50:53 -0500

    Update api.py

--- api.py
+++ api.py
@@ -1,12 +1,14 @@
 from flask import Flask,jsonify
 import random,logging
 from main import get_meme, check_image
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
 count = 0
+app.config['CORS_HEADERS'] = 'Content-Type'
 randommeme = ['meme','dankmeme','wholesomeme','memes']
 
-@app.route('/givememe')
+@app.route('/givememe',methods = ['GET'])
 def random_meme():              #Random and one
     sub = random.choice(randommeme)
     r = get_meme(sub,100)
@@ -23,3 +25,6 @@ def random_meme():              #Random and one
         'Redditurl': requsted["Redditurl"],
         'Subreddit': requsted["Subreddit"]
     })
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
